5c18b1b46af609235888feddf8776be2605b620a
kartik81687 <106053449+kartik81687@users.noreply.github.com>
2025-02-24 15:16:25 +0530

    commit

--- src/components/styledText.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const StyledText = ({ text }) => {
-
-    return <div className='text-black dark:text-white' dangerouslySetInnerHTML={{ __html: text }} />;
-};
-
-export default StyledText;
